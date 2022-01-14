485ab4c1e566bab87092798671b885b2ecf6a863
shayandesh <shafaghi1378@gmail.com>
2022-01-14 17:16:03 +0330

    Check memory access

--- kern/pmap.c
+++ kern/pmap.c
@@ -783,8 +783,26 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
-	return 0;
+    pte_t *pte;
+    uint64_t begin;
+    uint64_t end;
 
+    begin = ROUNDDOWN((uint64_t) va, PGSIZE);
+    end = ROUNDUP((uint64_t) va + len, PGSIZE);
+    perm |= PTE_P;
+        
+    while (begin < end) {
+
+        pte = pml4e_walk(env->env_pml4e, (void *) begin, 0);
+        if (!pte || *pte >= ULIM || ((*pte & perm) != perm)) {
+            user_mem_check_addr = begin < (uint64_t) va ? (uint64_t) va : begin; 
+            return -E_FAULT;
+        }
+
+        begin += PGSIZE;
+    }
+
+	return 0;
 }
 
 //
